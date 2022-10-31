eb8b0c01b06d57c3a00b7afa4328a287358553a0
mehedihasanroni <mehedihasanroni0852@gmail.com>
2022-10-31 15:19:43 +0600

    learned github

---
+++ TrueFalse.js
@@ -0,0 +1,10 @@
+//for false name or tags for javascript :"null,nan,0,''"
+//for trus name or tags for javascript :' '.
+const age = '';
+if (age) {
+    console.log('this is true name')
+    
+}
+else{
+    console.log('this is false name')
+}
